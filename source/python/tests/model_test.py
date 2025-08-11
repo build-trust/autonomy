eb4bf25cd889fdee1f30a1ea4cb6af80b4ea7b1e
Mrinal Wadhwa <mrinal@ockam.io>
2025-08-11 08:28:49 -0700

    Set up Ruff and format Python code

--- source/python/tests/model_test.py
+++ source/python/tests/model_test.py
@@ -2,8 +2,9 @@ import pytest
 
 from autonomy.models.model import Model
 
+
 class TestModel:
-    def test_constructor_sets_name_correctly(self):
-        expected = "test_model"
-        model = Model(expected)
-        assert model.name == expected
+  def test_constructor_sets_name_correctly(self):
+    expected = "test_model"
+    model = Model(expected)
+    assert model.name == expected
